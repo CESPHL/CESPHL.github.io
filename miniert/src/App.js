9f508c749eb39b88821c349808baaca58346e579
JeanneCL <charlizelapid12@gmail.com>
2023-11-09 20:39:29 +0800

    base forgot pass page

--- miniert/src/App.js
+++ miniert/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Login from './Components/Login/Login.jsx';
+import Screen1 from './Components/ForgotPass/Screen1.jsx';
 
 function App() {
   return (
     <div>
-      <Login/>
-    </div>
+     <Login/>
+     <Screen1/>
+     </div>
   );
 }
 
