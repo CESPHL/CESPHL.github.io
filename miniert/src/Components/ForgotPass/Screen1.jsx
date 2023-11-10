b7cba932f7441b82716bce499bb49ca1144b032e
russel-mempin <russelmempin216@gmail.com>
2023-11-10 15:37:37 +0800

    Front end changes

--- miniert/src/Components/ForgotPass/Screen1.jsx
+++ miniert/src/Components/ForgotPass/Screen1.jsx
@@ -20,42 +20,51 @@ export default function Screen1(){
         <div className = "forgot-form">
             <div className="rectangle">
                 <div className ="title">            
-                <div className = "main">
-                    <img src= {main} alt=""/><span>Collabera Digital <br/>
-                    External Resource Timesheet</span>
+                    <div className = "main">
+                        <img src= {main} alt=""/><span>Collabera Digital<br/>External Resource Timesheet</span>
                     </div>
-                <div className="list">
-                <div class="item">
-                    <img src = {person} alt=""/> 
-                    <div className="item1">
-                    <h5>Email confirmation </h5>
-                    <p> Provide your email for resent instructions.</p>
+                    <div className="list">
+                        <div class="item">
+                            <div>
+                                <img src = {person} alt=""/> 
+                                <div className="item1">
+                                    <h5>Email confirmation </h5>
+                                    <p>Provide your email for resent instructions.</p>
+                                </div>
+                            </div>
+                            <div>
+                                <img src = {nvmail} alt=""/>
+                                <div className="item2">
+                                    <h5>Email Sent</h5>
+                                    <p>Reset Password link has been sent.</p>
+                                </div>
+                            </div>
+                            <div>
+                                <img src ={bubble} alt=""/>
+                                <div className="item3">
+                                    <h5>New Password Details</h5>
+                                    <p>Input your new password in the field.</p>
+                                </div>
+                            </div>
+                            <div>
+                                <img src={check} alt=""/> 
+                                <div className="item4">
+                                    <h5>Reset Complete</h5>
+                                    <p>Your password has been updated.</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <img src = {nvmail} alt=""/>
-                    <div className="item2">
-                    <h5>Email Sent</h5>
-                    <p>Reset Password link has been sent.</p>
-                    </div>
-                    <img src ={bubble} alt=""/>
-                    <div className="item3">
-                    <h5>New Password Details</h5>
-                    <p>Input your new password in the field.</p>
-                    </div>
-                    <img src={check} alt=""/> 
-                    <div className="item4">
-                    <h5>Reset Complete</h5>
-                    <p>Your password has been updated.</p>
-                    </div>
-                </div>
-                </div>
                 </div>
             </div>
             <form action ="">
                 <div className ="padding">
-                <img src = {key}/><br/>
-                <span className="forgot"> Forgot Password? </span> 
+                    <img src = {key}/><br/>
+                    <span className="forgot"> Forgot Password? </span> 
                 </div>
-                <div className="subheading">Enter your email to receive reset instructions</div>
+                <div className="subheading">
+                    Enter your email to receive reset instructions
+                </div>
                 <div className="forgot-pass">
                     <label>Email</label> <br/>
                     <input type = "email" placeholder='Enter email' required/>
@@ -65,7 +74,6 @@ export default function Screen1(){
                     <button onClick={handleClick}><img src = {arrow} alt="arrow key"/><span>Back to Log In</span></button>
                     </div>
             </form>
-            </div>
-                
+            </div>    
     )
 }
