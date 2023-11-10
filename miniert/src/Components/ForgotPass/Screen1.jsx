637caca93841abbaad37140671afc59746dc1366
JeanneCL <charlizelapid12@gmail.com>
2023-11-10 11:12:20 +0800

    minor changes

--- miniert/src/Components/ForgotPass/Screen1.jsx
+++ miniert/src/Components/ForgotPass/Screen1.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import arrow from '../Assets/back_arrow.svg';
 import main from '../Assets/Framemain.svg';
 import key from '../Assets/Key.svg';
+import person from '../Assets/Person.svg';
+import nvmail from '../Assets/navbar_email.svg';
+import lock from '../Assets/Lock.svg';
+import fpassemail from '../Assets/forget-pass email.svg';
+import check from '../Assets/forget-pass check.svg';
+import bubble from '../Assets/desktopbubble.svg';
+import checkchange from '../Assets/Check_ring.svg';
+
 import './main.css';
 
 export default function Screen1(){
@@ -16,11 +24,23 @@ export default function Screen1(){
                     <img src= {main}/><span>Collabera Digital <br/>
                     External Resource Timesheet</span>
                     </div>
+                <div className="list">
+                <div class="item">
+                    <img src = {person} alt=""/> <span>Email confirmation</span><br/>
+                    <span className="subtitle"> Provide your email for resent instructions.</span><br/>
+                    <img src = {nvmail} alt=""/> <span> Email Sent</span><br/>
+                    <span className="subtitle">Reset Password link has been sent.</span><br/>
+                    <img src ={bubble} alt=""/><span> New Password Details</span><br/>
+                    <span className="subtitle">Input your new password in the field.</span><br/>
+                    <img src={check} alt=""/> <span>Reset Complete</span><br/>
+                    <span className="subtitle">Your password has been updated.</span>
+                </div>
+                </div>
                 </div>
             </div>
             <form action ="">
                 <div className ="padding">
-                <img src = {key}/><br/>
+                <img src = {key} alt=""/><br/>
                 <span className="forgot"> Forgot Password? </span> 
                 </div>
                 <div className="subheading">Enter your email to receive reset instructions</div>
