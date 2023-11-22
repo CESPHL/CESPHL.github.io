00b890fbbcc7e4df21d2b769e6c1072b2d40c5a6
russel-mempin <russelmempin216@gmail.com>
2023-11-22 09:57:22 +0800

    Completed functionality for Profile Removed console.log code for the backend

--- backend/controllers/talentsController.js
+++ backend/controllers/talentsController.js
@@ -54,7 +54,7 @@ const updateTalent = async (req, res) => {
         res.status(500).json({ message: 'Internal server error' });
     }
 }
-    
+
 // Find employee then check if there is an existing time in date.
 // If there is an existing date, don't save it and return a message.
 // If there is no existing date, save it to the database on a new column.
@@ -225,7 +225,41 @@ const timeOutOT = async (req, res) => {
 }
 
 const changePassword = async (req, res) => {
-    
+    const { employee_id } = req.params;
+    const { oldPassword, password } = req.body;
+    try {
+        const user = await Talent.findOne({ employee_id });
+
+        if (user) {
+            const passwordMatch = await bcrypt.compare(oldPassword, user.password);
+
+            // Password is qwerty12345!
+
+            if (passwordMatch === true && oldPassword != password) {
+                const hashedPassword = await bcrypt.hash(password, 10);
+                user.password = hashedPassword;
+                await user.save();
+                console.log("Password successfully changed.");
+                return res.status(200).json({ message: "Password successfully changed." });
+            }
+            else if (oldPassword == password) {
+                console.log("New password must be different from old password.");
+                return res.status(401).json({ message: "New password must be different from old password." });
+            }
+            else if (passwordMatch === false) {
+                console.log("Old password is incorrect.");
+                return res.status(401).json({ message: "Old password is incorrect." })
+            }
+            else {
+                console.log("Something went wrong.");
+                return res.status(400).json({ message: "Something went wrong." })
+            }
+        }
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 }
 
 module.exports = {
@@ -236,5 +270,6 @@ module.exports = {
     timeIn,
     timeOut,
     timeInOT,
-    timeOutOT
+    timeOutOT,
+    changePassword
 }
