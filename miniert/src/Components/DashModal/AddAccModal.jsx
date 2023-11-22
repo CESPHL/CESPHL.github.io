3835568312e2e3761559e5c53ba5d48025e07c4a
JeanneCL <charlizelapid12@gmail.com>
2023-11-22 16:32:25 +0800

    add account and manage acccounts

---
+++ miniert/src/Components/DashModal/AddAccModal.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './changePassModal.css';
+
+const ChangePassModal = ({ show, handleClose, children, handleSave }) => {
+  const showHideClassName = show ? "change-pass-modal change-pass-modal-display-block" : "change-pass-modal change-pass-modal-display-none";
+
+  return (
+    <div className={showHideClassName}>
+      <section className="change-pass-modal-main">
+        {children}
+
+        <div className="change-pass-modal-buttons-container">
+          <button className="change-pass-modal-close-btn" onClick={handleClose}>Close</button>
+          <button className="change-pass-modal-save-btn" onClick={handleClose}>Yes, add</button>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default ChangePassModal;
