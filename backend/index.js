e056abcddfaaae3997925d78560ed2febf75f914
CESPHL <CESPHL@collaberadigital.com>
2023-11-28 16:17:35 +0800

    TESTING IF BACKEND WORKS

--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,46 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors =  require('cors');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const corsOptions = {
-    origin: 'https://cesphl-github-io-backend.vercel.app/',
-    credentials: true,
-    optionalSuccessStatus:200
-}
+  origin: "https://cesphl-github-io-backend.vercel.app/",
+  credentials: true,
+  optionalSuccessStatus: 200,
+};
 
-const talentsRoutes = require('./routes/talents');
-const indexRoutes = require('./routes/index');
-const clientRoutes = require('./routes/clients');
-const managerRoutes = require('./routes/managers');
-const adminRoutes = require('./routes/admin');
+const talentsRoutes = require("./routes/talents");
+const indexRoutes = require("./routes/index");
+const clientRoutes = require("./routes/clients");
+const managerRoutes = require("./routes/managers");
+const adminRoutes = require("./routes/admin");
 
 const app = express();
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use((req, res, next) => {
-    console.log(req.path, req.method);
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
+  console.log(req.path, req.method);
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
 });
 
-app.use('/api/talents', talentsRoutes);
-app.use('/api/clients', clientRoutes);
-app.use('/', indexRoutes);
-app.use('/api/managers', managerRoutes);
-app.use('/api/admin', adminRoutes);
+app.use("/api/talents", talentsRoutes);
+app.use("/api/clients", clientRoutes);
+app.use("/", indexRoutes);
+app.use("/api/managers", managerRoutes);
+app.use("/api/admin", adminRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log("Test here");
-        app.listen(process.env.PORT || 3000, () => {
-            console.log(`Connected to db & listening on port ${process.env.PORT}`);
-        });
-    })
-    .catch((error) => {
-        console.log(error);
-    })
-;
+app.get("/", (req, res) => {
+  res.send("Hello, this is the root path!");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Connected to db & listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
