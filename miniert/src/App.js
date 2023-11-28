4f7d20da9e1e4133b54adf208f64fef22fe6c833
CESPHL <CESPHL@collaberadigital.com>
2023-11-28 16:38:23 +0800

    Changed old code for Render to Vercel

--- miniert/src/App.js
+++ miniert/src/App.js
@@ -25,7 +25,7 @@ function App() {
 	const [message, setMessage] = useState("");
 
 	useEffect(() => {
-		fetch("https://mini-ert.onrender.com")
+		fetch("https://cesphl-github-io-backend-m1syxzyze-dummy-lastnames-projects.vercel.app/")
 			.then((res) => res.json())
 			.then((data) => setMessage(data.message));
 	}, []);
