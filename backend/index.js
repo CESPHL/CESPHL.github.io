8f913711bf290e74597ad311c6ac0ed6d26325da
CESPHL <CESPHL@collaberadigital.com>
2023-11-29 10:07:16 +0800

    still troubleshooting frontend access to backend

--- backend/index.js
+++ backend/index.js
@@ -28,6 +28,10 @@ app.use("/", indexRoutes);
 app.use("/api/managers", managerRoutes);
 app.use("/api/admin", adminRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Hello, this is the root path!");
+});
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
