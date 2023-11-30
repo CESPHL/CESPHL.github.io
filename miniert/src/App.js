712eaa9aa46790b829fa552423d7d6ecda5d3074
CESPHL <CESPHL@collaberadigital.com>
2023-11-30 17:03:14 +0800

    fix route of displaying add user page

--- miniert/src/App.js
+++ miniert/src/App.js
@@ -19,6 +19,7 @@ import AddTalent from "./Components/ManagerDash/AddTalent.jsx";
 // Admin pages
 import AdminManageAccounts from "./Components/Admin/ManageAccounts.jsx";
 import AdminManageUsers from "./Components/Admin/ManageUsers.jsx";
+import AdminAddUsers from "./Components/Admin/AddUser.jsx";
 import AdminManageTalents from "./Components/Admin/ManageTalents";
 import AdminProfile from "./Components/Admin/Profile";
 
@@ -42,6 +43,7 @@ function App() {
         <Route path="/manage-talents/add-account" element={<AddTalent />} />
         <Route path="/admin/manage-accounts" element={<AdminManageAccounts />} />
         <Route path="/admin/manage-users" element={<AdminManageUsers />} />
+        <Route path="/admin/manage-users/add-user" element={<AdminAddUsers />} />
         <Route path="/admin/manage-talents" element={<AdminManageTalents />} />
         <Route path="/admin/profile" element={<AdminProfile />} />
       </Routes>
