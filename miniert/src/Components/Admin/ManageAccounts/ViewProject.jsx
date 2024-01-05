661900914f11cd7f674316109c29596d7cf02c61
CESPHL <CESPHL@collaberadigital.com>
2024-01-05 11:55:00 +0800

    imported icons

--- miniert/src/Components/Admin/ManageAccounts/ViewProject.jsx
+++ miniert/src/Components/Admin/ManageAccounts/ViewProject.jsx
@@ -9,6 +9,8 @@ import accIcon from '../../Assets/acc-active.svg';
 import talents from '../../Assets/mng-talent-inactive.svg';
 import users from "../../Assets/users-inactive.svg";
 import profile from '../../Assets/inactive-profile.svg';
+import view from "../../Assets/view-icn.svg";
+import edit from "../../Assets/edit-icn.svg";
 
 // External functionalities
 import axios from "axios";
